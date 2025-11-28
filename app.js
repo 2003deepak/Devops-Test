a2ddee777dfe8511221e8834b2610f8cd96bd202
= <=>
2025-11-28 14:35:12 +0530

    Merge CHnages

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is deepak running at http://localhost:${port}`);
 });
 
 module.exports = app;
